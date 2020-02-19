c17a1c545fc37ad7f83b7f0a5686332c3bc62905
john <jpatek@ubiqsecurity.com>
2020-02-18 22:13:37 -0800

    Finished fa.cpp

--- CS421Progs/HW2BFiles/fa.cpp
+++ CS421Progs/HW2BFiles/fa.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 #include<fstream>
 #include<string>
-using namespace std;
 
 // CS421 File fa.cpp for HW2B
 
 //-----------------------------------------------------------------
-// Your name: **
+// Your name: John Patek
 
 // ** Change this to fit the HW3C specification - look for **
 // ** Must have the same types of tracing couts as mytoken.
@@ -15,114 +14,199 @@ using namespace std;
 
 // MYTOKEN DFA done by Rika Sensei has a sample
  // This FA is for a b^+
-bool mytoken(string s)
+bool mytoken(const std::string s)
 { 
-  int state = 0;
-  int charpos = 0;
+    int state = 0;
+    int charpos = 0;
 
-  cout << "Trying the mytoken machine..." << endl;  
- 
-  while (s[charpos] != '\0') 
+    std::cout << "Trying the mytoken machine..." << std::endl;  
+
+    while (s[charpos] != '\0') 
     {
-      cout << "current state: " << state << endl;
-      cout << "character: " << s[charpos] << endl;
- 
-      if (state == 0 && s[charpos] == 'a')
-      state = 1;
-      else
-      if (state == 1 && s[charpos] == 'b')
-      state = 2;
-      else
-      if (state == 2 && s[charpos] == 'b')
-      state = 2;
-      else
-	{
-	  cout << "I am stuck in state " << state << endl;
-	  return(false);
-	}
-      charpos++;
+        std::cout << "current state: " << state << std::endl;
+        std::cout << "character: " << s[charpos] << std::endl;
+
+        if (state == 0 && s[charpos] == 'a')
+        {
+            state = 1;
+        }
+        else if (state == 1 && s[charpos] == 'b')
+        {
+            state = 2;
+        }
+        else if (state == 2 && s[charpos] == 'b')
+        {
+            state = 2;
+        }
+        else
+        {
+            std::cout << "I am stuck in state " << state << std::endl;
+            return(false);
+        }
+        charpos++;
     }//end of while
 
-  // where did I end up????
-  if (state == 2) return(true);  // end in a final state
-   else return(false);
+    // where did I end up????
+    if (state == 2) 
+    {
+        return(true);  // end in a final state
+    }
+    else
+    { 
+        return(false);
+    }
 }
 
 
 // IDENT DFA 
  // This FA is for **
-bool ident_token(string s)
+bool ident_token(const std::string & s)
 {
-  // ** complete this based on mytoken
+    bool result(false);
+    std::string::const_iterator string_iterator;
+    if(s.size() > 0)
+    {
+        string_iterator = s.begin();
+        result = (*string_iterator >= 'a' 
+               && *string_iterator <= 'z')
+               || (*string_iterator >= 'A' 
+               && *string_iterator <= 'Z')
+               || *string_iterator == '_';
+        string_iterator++;
+        while(result && string_iterator != s.end())
+        {
+            result = (*string_iterator >= '0' 
+                   && *string_iterator <= '9')
+                   || (*string_iterator >= 'a' 
+                   && *string_iterator <= 'z')
+                   || (*string_iterator >= 'A' 
+                   && *string_iterator <= 'Z')
+                   || *string_iterator == '_';
+            string_iterator++;
+        }
+    }
+    return result;
 }
 
 
 // REAL DFA 
 // This FA is for **
-bool real_token(string s)
+bool real_token(const std::string & s)
 {
-  // ** complete this based on mytoken
+    bool result(false);
+    std::string::const_iterator string_iterator;
+    if(s.size() > 0 && s.find('.') != std::string::npos)
+    {
+        if(s.find_first_of('.') == s.find_last_of('.'))
+        {
+            string_iterator = s.begin();
+            result = (*string_iterator >= '0' 
+                && *string_iterator <= '9');
+            string_iterator++;
+            while(result && string_iterator != s.end())
+            {
+                result = (*string_iterator >= '0' 
+                    && *string_iterator <= '9')
+                    || (*string_iterator == '.' 
+                    && string_iterator + 1 != s.end());
+                string_iterator++;
+            }
+        }
+    }
+    return result;
 }
 
 
 //INT DFA 
 // This FA is for **
-bool integer_token(string s)
+bool integer_token(const std::string & s)
 {
-  // ** complete this based on mytoken
+    bool result(false);
+    std::string::const_iterator string_iterator;
+    if(s.size() > 0)
+    {
+        string_iterator = s.begin();
+        result = (*string_iterator >= '0' 
+               && *string_iterator <= '9');
+        string_iterator++;
+        while(result && string_iterator != s.end())
+        {
+            result = (*string_iterator >= '0' 
+                   && *string_iterator <= '9');
+            string_iterator++;
+        }
+    }
+    return result;
 }
 
 
 // -----------------------------------------------------
 
 enum tokentype {ERROR, MYTOKEN, IDENT, REAL, INT};
-int scanner(tokentype& , string& );  // to be called by main
-fstream fin;   // file stream to use
+void scanner(tokentype& , std::string& );  // to be called by main
+std::fstream fin;   // file stream to use
 
 // The test-driver
 int main()
-{ string fname;
-  cout << "Enter the input file name:";
-  cin >> fname; 
+{ 
+    std::string fname;
+    std::cout << "Enter the input file name:";
+    std::cin >> fname; 
 
-  fin.open( fname.c_str(), fstream::in);
+    fin.open( fname.c_str(), std::fstream::in);
 
-  string tokens[5] = {"ERROR", "MYTOKEN", "IDENT", "REAL", "INT"};
-  tokentype thetype;
-  string theword; 
-
-  while (true)  // keep on going 
+    std::string tokens[5] = {"ERROR", "MYTOKEN", "IDENT", "REAL", "INT"};
+    tokentype thetype;
+    std::string theword; 
+    bool loop(true);
+    while(loop)
     {
-      scanner(thetype, theword);  // the paramers will be set by Scanner
-      if (theword == "EOF") break;
-
-      cout << "Type is:" << tokens[thetype]  << endl;
+        scanner(thetype, theword);  // the paramers will be set by Scanner
+        if (theword == "EOF")
+        {
+            loop = false;
+        }
+        else
+        {
+            std::cout << "Type is:" << tokens[thetype]  << std::endl;
+        }
     }
-  
-  fin.close();
+    fin.close();
 }// end of main
 
 
 // sets the_type and w 
-int scanner(tokentype& the_type, string& w)
+void scanner(tokentype& the_type, std::string& w)
 {
-  
-  // This goes through all machines one by one on the input string w
 
-  cout << ".....Scanner was called..." << endl;
+    // This goes through all machines one by one on the input string w
 
-  fin >> w;  // grab next word from fain.txt
-  cout << "Word is:" << w << endl;   
+    std::cout << ".....Scanner was called..." << std::endl;
 
-  if (mytoken(w))
-    { the_type = MYTOKEN; }
-  
-  // ** add other if-then's here in the right order to go through
-  // ** all FAs one by one and set the_type to be IDENT, REAL or INT.
+    fin >> w;  // grab next word from fain.txt
+    std::cout << "Word is:" << w << std::endl;   
 
-  else //none of the FAs returned TRUE
-    { cout << "Lexical Error: The string is not in my language" << endl;
-      the_type = ERROR; }
+    if (mytoken(w))
+    { 
+        the_type = MYTOKEN; 
+    }
+    else if(ident_token(w))
+    {
+        the_type = IDENT;
+    }
+    else if(real_token(w))
+    {
+        the_type = REAL;
+    }
+    else if(integer_token(w))
+    {
+        the_type = INT;
+    }
+    else //none of the FAs returned TRUE
+    { 
+        std::cout << "Lexical Error: The string is not in my language" << std::endl;
+        the_type = ERROR; 
+    }
 
 }//the very end of scanner
 
