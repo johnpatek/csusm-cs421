bcdbe845dbbd0b6565fef0ca295b88059a5e199b
john <jpatek@ubiqsecurity.com>
2020-02-18 19:33:53 -0800

    Finished HW1BFiles

--- CS421Progs/HW1BFiles/generator.cpp
+++ CS421Progs/HW1BFiles/generator.cpp
@@ -1,14 +1,57 @@
-#include<iostream>
-#include<string>
-using namespace std;
-// include queue.h or equivalent 
+#include <iostream>
+#include <string>
+#include <list>
+#include <cmath>
+
+double log_base(const double value, const double base)
+{
+    return log(value) / log(base);
+}
+
+/*
+This is a more advanced version of what has been described below.
+Each decimal number will be encoded as a trinary number and then its
+printed.
+
+0 -> 0
+1 -> 1
+2 -> 2
+3 -> 10
+4 -> 11
+5 -> 12
+6 -> 20
+7 -> 21
+8 -> 22
+9 -> 100
+...
+
+*/
+void encode_and_print(
+    const int decimal, 
+    std::ostream & stream = std::cerr)
+{
+    if(decimal > 0)
+    {
+        int order = (int)log_base(decimal,3);
+        while(order >= 0)
+        {
+            stream << (decimal/(int)pow(3,order)) % 3;
+            order--;
+        }
+    }
+    else
+    {
+        stream << 0;
+    }
+    stream << std::endl;
+}
 
 // CS421 HW1 Write a generator in C++ for L = {x | x is a binary number}.
 
-// Your name:
+// Your name: John Patek
 
 // Copy the recognizer function here from the other file.
-
+// Not necessary since it the generator has a different alphabet
 
 // main: It should create each string over E = {0,1,2} systematically
 //   (short to long) and pass each string to the recognizer function
@@ -18,12 +61,13 @@ using namespace std;
 //   Keeps on going until the queue overflows but the user can
 //   terminate the program with control-C 
 //   after 20 strings have been displayed.
-int main()
+int main(const int argc, const char ** argv)
 {
-
-  while(true)
+    int number = 0;
+    while (number < 100)
     {
-
+        encode_and_print(number);
+        number++;
     }
-
- }
+    return 0;
+}
--- CS421Progs/HW1BFiles/recognizer.cpp
+++ CS421Progs/HW1BFiles/recognizer.cpp
@@ -1,19 +1,47 @@
 #include<iostream>
 #include<string>
-using namespace std;
 
 // CS421 HW1 Write a recognizer in C++ for L = {x | x is a binary number}.
 
-// Your name:
+// Your name: John Patek
 
 //  The recognizer function should return TRUE or FALSE 
 //  checking each character
 //  to make sure it is 0 or 1. (leading 0's are allowed).
-bool recognizer(string s)
-{}
+bool recognizer(const std::string& s)
+{
+    bool result(false);
+    if(s.size() > 0)
+    {
+        std::string::const_iterator string_iterator = s.begin();
+        result = *string_iterator == '1';
+        
+        if(s.size() > 1)
+        {
+            string_iterator = std::next(string_iterator);
+            while(result && string_iterator != s.end())
+            {
+                result = *string_iterator == '0' || *string_iterator == '1';
+                string_iterator = std::next(string_iterator);
+            }
+        }
+    }
+    return result;
+}
 
 //main: Given a string (from E) cined from the user, pass it to
 //      the recognizer function.
 //      Cout YES IN L or NO NOT IN L depending on what was returned.
-int main()
-{}
+int main(const int argc, const char ** argv)
+{
+    std::string message("NO NOT IN L");
+    std::string input;
+    std::cerr << "Enter a string: ";
+    std::cin >> input;
+    if(recognizer(input))
+    {
+        message = "YES IN L";
+    }
+    std::cerr << message << std::endl;
+    return 0;
+}
